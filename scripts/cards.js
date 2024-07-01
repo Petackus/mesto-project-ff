f9178ed0fff3d4ae2c23be991c5733e4b8cae024
petackus <petackus@mail.ru>
2024-07-01 18:44:56 +0300

    First Try

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,5 +1,5 @@
 const initialCards = [
-    {
+    { 
       name: "Архыз",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
     },
@@ -24,3 +24,4 @@ const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     }
 ];
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+function deleteCard(evt){
+   const item = evt.target.closest('.card');
+   item.remove()};
+
+
+
+initialCards.forEach(function(card){
+ addCard(card)
+}) 
+ function addCard (card){
+   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const title = cardElement.querySelector('.card__title');
+    const image = cardElement.querySelector('.card__image');
+    if (card.link) {
+      image.src = card.link;
+   }
+    if (card.name) {
+      title.textContent = card.name;
+    }
+ 
+ if (card.link && card.name)
+     {image.setAttribute('alt', card.name)
+   }
+
+   cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+   const placesList = document.querySelector('.places__list');
+   placesList.append(cardElement);
+ }
+
+// const addButton = document.querySelector('.profile__add-button');
+//  addButton.addEventListener('click', function () {
+//  });
 
 // @todo: Вывести карточки на страницу
